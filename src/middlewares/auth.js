ea50531dee3538e97b7bae08479f81af47fedb9d
adryeldeev <adryeldev@hotmail.com>
2025-06-26 13:40:10 -0300

    authorization admin

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,21 +2,19 @@ import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).json({ error: 'Access token is missing' });
-    }
+    if (!token) return res.status(401).json({ error: 'Access token missing' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Invalid access token' });
-        }
-
-        if (user.role !== "admin") {
-            return res.status(403).json({ message: "Acesso negado" });
-        }
+        if (err) return res.status(403).json({ error: 'Invalid token' });
 
         req.user = user;
         next();
     });
+};
+
+export const authorizeAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Acesso negado: admin only' });
+    }
+    next();
 };
--- src/routes/leadRoutes.js
+++ src/routes/leadRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getLeads, addLead, updateLead, deleteLead } from '../controllers/leadController.js';
-import { authenticateToken } from '../middlewares/auth.js';
+import { authenticateToken, authorizeAdmin } from '../middlewares/auth.js';
 const router = express.Router();
 
 // Define the routes for lead management
-router.get('/lead', authenticateToken, getLeads); // Get all leads
+router.get('/lead',authorizeAdmin, authenticateToken, getLeads); // Get all leads
 router.post('/lead', addLead); // Create a new lead
 router.put('/lead/:id',authenticateToken, updateLead); // Update a lead by ID
 router.delete('/lead/:id',authenticateToken, deleteLead); // Delete a lead by ID
